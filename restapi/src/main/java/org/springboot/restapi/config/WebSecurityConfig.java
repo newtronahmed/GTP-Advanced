41a40d083b7bcaef4c1783a3eb54263e3c1f846d
ahmed.zubairu <bazubairu@st.ug.edu.gh>
2024-08-24 13:39:22 +0000

    feat: lab on in working progress

--- restapi/src/main/java/org/springboot/restapi/config/WebSecurityConfig.java
+++ restapi/src/main/java/org/springboot/restapi/config/WebSecurityConfig.java
@@ -2,14 +2,20 @@ package org.springboot.restapi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -32,16 +38,41 @@ public class WebSecurityConfig {
 
         return http.build();
     }
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider(JdbcDaoImpl userDetailsService, PasswordEncoder encoder){
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(encoder);
+        return provider;
+    }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
-
-        return new InMemoryUserDetailsManager(user);
+    public JdbcDaoImpl userDetailsService(DataSource dataSource) {
+        JdbcDaoImpl jdbcDaoImpl = new JdbcDaoImpl();
+        if (dataSource != null) {
+            jdbcDaoImpl.setDataSource(dataSource);
+        } else {
+            throw new RuntimeException("DataSource is null!");
+        }
+        return jdbcDaoImpl;
     }
+
+
+    @Bean
+    public PasswordEncoder encoder() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        return encoder;
+    }
+//
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        UserDetails user =
+//                User.withDefaultPasswordEncoder()
+//                        .username("user")
+//                        .password("password")
+//                        .roles("USER")
+//                        .build();
+//
+//        return new InMemoryUserDetailsManager(user);
+//    }
 }
