bf1923488d1cf3837ea60e59dff83c48751f0f6b
ahmed.zubairu <bazubairu@st.ug.edu.gh>
2024-08-31 21:48:30 +0000

    feat: add cache config

--- hms/src/main/java/org/springboot/hms/config/CacheConfig.java
+++ hms/src/main/java/org/springboot/hms/config/CacheConfig.java
@@ -1,7 +1,6 @@
 package org.springboot.hms.config;
 
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
